cdb1b352069b1397aba655e6db515c06f108d831
ga1ker <zenget008@gmail.com>
2025-03-13 18:05:41 -0600

    Get properties from backend and show in the frontend

--- app/components/Categories.tsx
+++ app/components/Categories.tsx
@@ -1,20 +1,20 @@
 import Image from 'next/image';
 const Categories = () => {
     return(
-        <div className="pt-3 cursor-pointer pb-6 flex items-center space-x-12">
-            <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100 hover:border-gray-200">
+        <div className="pt-3 pb-6 flex items-center space-x-12">
+            <div className="cursor-pointerpb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100 hover:border-gray-200">
                 <Image src="/icn_category_beach.jpeg" alt="Category" width={20} height={20} />
                 <span className='text-xs'>Beach</span>
             </div>
-            <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100 hover:border-gray-200">
+            <div className="cursor-pointer pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100 hover:border-gray-200">
                 <Image src="/icn_category_beach.jpeg" alt="Category" width={20} height={20} />
                 <span className='text-xs'>Villas</span>
             </div>
-            <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100 hover:border-gray-200">
+            <div className="cursor-pointer pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100 hover:border-gray-200">
                 <Image src="/icn_category_beach.jpeg" alt="Category" width={20} height={20} />
                 <span className='text-xs'>Cabins</span>
             </div>
-            <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100 hover:border-gray-200">
+            <div className="cursor-pointer pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100 hover:border-gray-200">
                 <Image src="/icn_category_beach.jpeg" alt="Category" width={20} height={20} />
                 <span className='text-xs'>Tiny homes</span>
             </div>
