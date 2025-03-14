f23eb9c41b344217d2adc1cd35f85e58d8d3e6d3
Gadiel Alcazar Bernal <galkker@192.168.1.66>
2025-03-14 16:33:26 -0600

    Add property button and modal

--- app/components/navbar/AddPropertyButton.tsx
+++ app/components/navbar/AddPropertyButton.tsx
@@ -1,6 +1,27 @@
-const AddPropertyButton = () => {
+'use client'
+import useAddPropertyModal from "@/app/hooks/useAddPropertyModal"
+import useLoginModal from "@/app/hooks/useLoginModal"
+
+interface AddPropertyButtonProps {
+    userId?: string | null;
+}
+
+const AddPropertyButton: React.FC<AddPropertyButtonProps> = ({
+    userId
+}) => {
+    const loginModal = useLoginModal();
+    const addPropertymodal = useAddPropertyModal()
+
+    const airbnbYourHome = () => {
+        if (userId) {
+            addPropertymodal.open();
+        } else {
+            loginModal.open();
+        }
+    }
+
     return (
-        <div className="p-2 cursor-pointer text-sm font-semibold rounded-full hover:bg-gray-100">
+        <div onClick={airbnbYourHome} className="p-2 cursor-pointer text-sm font-semibold rounded-full hover:bg-gray-100">
             DjangoBnb Your home
         </div>
     )
