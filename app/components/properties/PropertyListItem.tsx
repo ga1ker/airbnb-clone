afe10745a411c34c171e1c0d1b2e3afaa4336a7e
ga1ker <zenget008@gmail.com>
2025-03-19 23:27:12 -0600

    Detail page for properties

--- app/components/properties/PropertyListItem.tsx
+++ app/components/properties/PropertyListItem.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import { PropertyType } from './PropertyList';
+import { useRouter } from 'next/navigation';
 
 interface PropertyProps {
     property: PropertyType
@@ -8,9 +9,9 @@ interface PropertyProps {
 const PropertyListItem: React.FC<PropertyProps> = ({
     property
 }) => {
-
+    const router=useRouter()
     return (
-        <div className="cursor-pointer">
+        <div className="cursor-pointer" onClick={() => router.push(`properties/${property.id}`)}>
             <div className="relative overflow-hidden aspect-square rounded-xl">
                 <Image fill src={property.image_url} sizes='(max-width: 768px) 768px, (max-width: 1200px): 768px, 768px' className='hover:scale-110 object-cover transition h-full w-full' alt='Beach house' />
             </div>
--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,46 +1,54 @@
 import ReservationSideBar from '@/app/components/properties/ReservationSideBar';
 import Image from 'next/image';
-const PropertyDetailPage = () => {
+
+import apiService from '@/app/services/apiService';
+
+const PropertyDetailPage = async ({params}: {params: {id: string}}) => {
+    const property = await apiService.get(`/api/properties/${params.id}`)
     return (
         <main className="max-w-[1500px] mx-auto px-6 pb-6">
             <div className="w-full h-[64vh] mb-4 overflow-hidden rounded-xl relative">
                 <Image 
                     fill
                     alt='Beach house' 
-                    src='/beach_1.jpg'
+                    src={property.image_url}
                     className='object-cover w-full h-full' />
             </div>
 
             <div className='mt-4 grid grid-cols-1 md:grid-cols-5 gap-4'>
                 <div className='py-6 pr-6 col-span-3'>
-                    <h1 className='mt-4 text-4xl'>Property name</h1>
+                    <h1 className='mt-4 text-4xl'>{property.title}</h1>
 
                     <span className='mb-6 block text-lg text-gray-600'>
-                        4 guests - 2 bedrooms - 1 bathroom
+                        {property.guests} guests - {property.bedrooms} bedrooms - {property.bathrooms} bathrooms
                     </span>
 
                     <hr />
 
                     <div className='py-6 flex items-center space-x-4'>
-                        <Image 
-                            src="/profile_pic_1.jpg"
-                            alt='Profile photo'
-                            width={50}
-                            height={50}
-                            className='rounded-full'/>
-                        <p><strong>Jon Doe</strong> es tu host</p>
+                        {
+                            property.landlord.avatar && (
+                                <Image 
+                                    src={property.landlord.image_url}
+                                    alt='Profile photo'
+                                    width={50}
+                                    height={50}
+                                    className='rounded-full'/>
+                            )
+                        }
+                        <p><strong>{property.landlord.name}</strong> es tu host</p>
                     </div>
 
                     <hr />
 
                     <p className='mt-6 text-lg'>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam iure architecto officiis nihil quam facilis delectus incidunt. Temporibus ea quidem tenetur, libero hic iure nesciunt quis sed nihil. Saepe, beatae.
-                        Vitae necessitatibus, ab eveniet omnis perferendis expedita molestias error nobis exercitationem adipisci earum, odio modi libero odit quia. Corporis, eveniet. Assumenda corporis asperiores animi reprehenderit alias maiores similique recusandae aut.
-                        Officiis reiciendis ut quia mollitia quam expedita quo rerum, est autem sed iure molestias beatae! Eum sed sint error molestias saepe, dignissimos quas eos rerum laboriosam neque placeat nobis facilis?
+                        {property.description}
                     </p>
                 </div>
 
-                <ReservationSideBar />
+                <ReservationSideBar
+                    property={property}
+                />
             </div>
         </main>
     )
